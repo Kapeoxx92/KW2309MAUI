d72ce15d3b8fa8e2a46b74ef028ead97005d60d5
1698551 <1698551@TE-GDA-D-D21E10>
2024-12-04 11:54:50 +0100

    finalCommit

--- KW2309MAUI/MainPage.xaml.cs
+++ KW2309MAUI/MainPage.xaml.cs
@@ -2,48 +2,67 @@
 {
     public partial class MainPage : ContentPage
     {
-        int gameResult = 0;
+        private const int NumberOfDice = 5;
+        private const int MaxDiceValue = 6;
+        private Random random;
+        private int gameResult = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            random = new Random();
         }
 
         private void DiceRoll(object sender, EventArgs e)
         {
-            int[] dice = new int[5];
-            Random r = new Random();
-            for (int i = 0; i < 5; i++)
+            int[] dice = new int[NumberOfDice];
+            for (int i = 0; i < NumberOfDice; i++)
             {
-                dice[i] = r.Next(1, 7);
+                dice[i] = random.Next(1, MaxDiceValue + 1);
             }
 
-            dice_0.Source = "k6_" + dice[0] + ".png";
-            dice_1.Source = "k6_" + dice[1] + ".png";
-            dice_2.Source = "k6_" + dice[2] + ".png";
-            dice_3.Source = "k6_" + dice[3] + ".png";
-            dice_4.Source = "k6_" + dice[4] + ".png";
+           
+            dice_0.Source = $"k6_{dice[0]}.png";
+            dice_1.Source = $"k6_{dice[1]}.png";
+            dice_2.Source = $"k6_{dice[2]}.png";
+            dice_3.Source = $"k6_{dice[3]}.png";
+            dice_4.Source = $"k6_{dice[4]}.png";
 
-            int rollResult = 0;
-            for (int dots = 1; dots <= 6; dots++)
-            {
-                int count = 0;
-                for (int diceIndex = 0; diceIndex < 5; diceIndex++)
-                {
-                    if (dice[diceIndex] == dots)
-                    {
-                        count++;
-                    }
-                }
-                if (count > 1)
-                {
-                    rollResult += dots * count;
-                }
-            }
+            int rollResult = CalculateRollResult(dice);
 
-            RollResultLabel.Text = "Wynik tego losowania: " + rollResult;
+            RollResultLabel.Text = $"Wynik tego losowania: {rollResult}";
             gameResult += rollResult;
-            GameResultLabel.Text = "Wynik gry: " + gameResult;
+            GameResultLabel.Text = $"Wynik gry: {gameResult}";
+        }
+
+        private int CalculateRollResult(int[] dice)
+        {
+            int rollResult = 0;
+
+            int[] counts = new int[MaxDiceValue + 1];
+
+            foreach (var die in dice)
+            {
+                counts[die]++;
+            }
+
+            for (int value = 1; value <= MaxDiceValue; value++)
+            {
+                if (counts[value] >= 3)
+                {
+                    rollResult += value * 3;
+                }
+                else if (counts[value] == 2)
+                {
+                    rollResult += value * 2;
+                }
+                else if (counts[value] == 1)
+                {
+                    rollResult += value;
+                }
+            }
+
+            return rollResult;
         }
 
         private void ResetGame(object sender, EventArgs e)
@@ -55,7 +74,7 @@
             dice_4.Source = "k6_0.png";
 
             gameResult = 0;
-            GameResultLabel.Text = "Wynik gry: " + gameResult;
+            GameResultLabel.Text = $"Wynik gry: {gameResult}";
             RollResultLabel.Text = "Wynik tego losowania: 0";
         }
     }
